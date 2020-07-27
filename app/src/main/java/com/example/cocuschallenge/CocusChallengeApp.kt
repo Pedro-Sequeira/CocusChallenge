170ef33e1335995944e0bfe0d0d9fb6280ca193c
Pedro Sequeira <pmcsequeira@gmail.com>
2020-07-27 19:59:28 +0100

    Hilt, Navigation and Gradle
    Removed .idea folder from git since it's local configurations.
    Added Hilt to the project.
    Added Navigation to the project with only a simple fragment to begin.
    Gradle dependencies versions organised all in one place.

---
+++ app/src/main/java/com/example/cocuschallenge/CocusChallengeApp.kt
@@ -0,0 +1,7 @@
+package com.example.cocuschallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CocusChallengeApp : Application()
--- app/src/main/java/com/example/cocuschallenge/MainActivity.kt
+++ app/src/main/java/com/example/cocuschallenge/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.cocuschallenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.cocuschallenge.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
